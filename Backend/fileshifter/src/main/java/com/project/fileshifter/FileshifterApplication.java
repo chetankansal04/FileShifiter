9846c94dcc0ac735dc34c05b39a5e627e1d62e41
chetankansal04 <ckansal0@gmail.com>
2025-09-08 20:56:42 +0530

    Env Update

--- Backend/fileshifter/src/main/java/com/project/fileshifter/FileshifterApplication.java
+++ Backend/fileshifter/src/main/java/com/project/fileshifter/FileshifterApplication.java
@@ -1,13 +1,20 @@
 package com.project.fileshifter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
+@EnableAutoConfiguration
 public class FileshifterApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(FileshifterApplication.class, args);
+
 	}
 
 }
